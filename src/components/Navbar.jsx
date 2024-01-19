1ad4c430609de1da2c9e58ca4dd6278b474e0e2a
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-01-19 22:01:39 +0530

    Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,70 @@
 import React from "react";
 import styled from "styled-components";
+import {SearchTwoTone} from "@mui/icons-material"
 
 const Container = styled.div`
   height: 60px;
 `;
+
 const Wrapper = styled.div`
   padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
+
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  `;
+
+  const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+  `;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+
 
 const Navbar = () => {
   return (
     <Container>
-      <Wrapper>Navbar</Wrapper>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input/>
+            <SearchTwoTone/>
+          </SearchContainer>
+        </Left>
+        <Center><Logo>NIGHTowls</Logo></Center>
+        <Right>right</Right>
+      </Wrapper>
     </Container>
   );
 };
