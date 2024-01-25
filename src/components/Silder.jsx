a2ba5e8528744963b1b9985b0bf71fa71b8c48e5
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-01-25 23:05:25 +0530

    Update Silder.jsx

--- src/components/Silder.jsx
+++ src/components/Silder.jsx
@@ -13,24 +13,26 @@ const Container = styled.div`
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
-  background-color: #efe3e3;
+  background-color: #fff7f7;
   border-radius: 50%;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   position: absolute;
   top: 0;
   bottom: 0;
-  left: ${props=> props.direction === "left" && "10px"};
-  right: ${props=> props.direction === "right" && "10px"};
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
     height: 100%;
-`
+    display: flex;
+`;
 
 const Slider = () => {
   return (
