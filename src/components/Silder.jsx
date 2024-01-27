f15fbd7a3f60b9651add53376b682f4756f1c5fe
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-01-27 22:38:32 +0530

    Update Silder.jsx

--- src/components/Silder.jsx
+++ src/components/Silder.jsx
@@ -6,18 +6,18 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: #de7309;
+  background-color: #f8f6f5;
   position: relative;
 `;
 
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
-  background-color: #fff7f7;
+  background-color: #efe3e3;
   border-radius: 50%;
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   position: absolute;
   top: 0;
   bottom: 0;
@@ -26,12 +26,46 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
-  z-index: 2;
 `;
 
 const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  cursor: pointer;
+  background-color: transparent;
 `;
 
 const Slider = () => {
@@ -40,6 +74,18 @@ const Slider = () => {
       <Arrow direction="left">
         <ArrowBackIosNewOutlinedIcon />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Winter Sale</Title>
+            <Desc>Get a Flat 30% Off The New Arrivals.</Desc>
+            <Button>Show Now</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowForwardIosOutlinedIcon />
       </Arrow>
