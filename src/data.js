8ca5a4472c22fa305c4eab899fba5a7c359fc340
SingAyush <anonymoussingh5665@agmail.com>
2024-01-30 11:01:53 +0530

    Slider Done

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
+      title: "Winter Sale",
+      desc: "Get a Flat 30% Off The New Arrivals.",
+      bg: "f6cdce",
+    },
+    {
+      id: 2,
+      img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
+      title: "Exclusive Sale",
+      desc: "Get a Flat 30% Off The New Arrivals.",
+      bg: "f5fafd",
+    },
+    {
+      id: 3,
+      img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
+      title: "Autumn Sale",
+      desc: "Get a Flat 30% Off The New Arrivals.",
+      bg: "faf8ed",
+    },
+  ]
+
+  
