69ee3ea6829cbcef208eed5c201b0108e7051cd2
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-01-31 15:23:47 +0530

    Data.js
    ***
    Contains data about the sliders and Categories
    ***

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
+      title: "Winter Sale",
+      desc: "Get a Flat 30% Off The New Arrivals.",
+      bg: "f6cdce",
+    },
+    {
+      id: 2,
+      img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
+      title: "Exclusive Sale",
+      desc: "Get a Flat 30% Off The New Arrivals.",
+      bg: "f5fafd",
+    },
+    {
+      id: 3,
+      img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
+      title: "Autumn Sale",
+      desc: "Get a Flat 30% Off The New Arrivals.",
+      bg: "faf8ed",
+    },
+  ]
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
