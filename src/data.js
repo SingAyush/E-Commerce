f60ac7b31b548d07edfc2253ec439a96ff4cee98
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-02-08 22:56:31 +0530

    data.js

--- src/data.js
+++ src/data.js
@@ -10,14 +10,14 @@ export const sliderItems = [
       id: 2,
       img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
       title: "Exclusive Sale",
-      desc: "Get a Flat 30% Off The New Arrivals.",
+      desc: "Get a Flat 35% Off The New Arrivals.",
       bg: "f5fafd",
     },
     {
       id: 3,
       img: "https://st.depositphotos.com/2097531/2144/i/450/depositphotos_21440313-stock-photo-attractive-man-dressed-casual-wearing.jpg",
       title: "Autumn Sale",
-      desc: "Get a Flat 30% Off The New Arrivals.",
+      desc: "Get a Flat 40% Off The New Arrivals.",
       bg: "faf8ed",
     },
   ]
@@ -26,12 +26,12 @@ export const sliderItems = [
     {
       id: 1,
       img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "SHIRT STYLE!",
+      title: "SHIRT STYLE!!",
     },
     {
       id: 2,
       img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "LOUNGEWEAR LOVE",
+      title: "LOUNGEWEAR",
     },
     {
       id: 3,
