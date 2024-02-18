09378109ba393725193227396520b2a8fb1e7768
SingAyush <anonymoussingh5665@agmail.com>
2024-02-18 14:03:31 +0530

    Footer additiion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Facebook, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -5,7 +6,20 @@ const Container = styled.div`
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
 `;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div``;
+
 const Right = styled.div`
   flex: 1;
 `;
@@ -16,7 +30,29 @@ const Center = styled.div`
 const Footer = () => {
   return (
     <Container>
-      <Left></Left>
+      <Left>
+        <Logo>NIGHTowls</Logo>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus,
+          facere? Dolores ullam nemo quibusdam! Delectus eum quas impedit
+          perspiciatis? In eum quae sint laborum nostrum esse officia, ea dolor
+          placeat.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon>
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon>
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon>
+            <LinkedIn />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
       <Right></Right>
       <Center></Center>
     </Container>
