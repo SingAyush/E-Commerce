cb74ef41a126d18f9b4c0b1357daaf8632a22a2a
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-03-13 16:15:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import Login from "./Pages/Login";
-// import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
 
 const App = () => {
-  return <Login />;
+  return <Cart />;
 };
 
 export default App;
